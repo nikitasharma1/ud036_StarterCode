e6fa1e49d426c8f3e2bec3a8421ade7d32cfb05b
Nikita Sharma <nikitasharma9010@gmail.com>
2017-09-13 20:45:31 +0530

    Add comments to class "Movie" in media.py

--- media.py
+++ media.py
@@ -1,5 +1,9 @@
 class Movie:
+    """ Model of a movie object """
+
     def __init__(self, title, storyline, poster_url, trailer_url):
+        """ Initialising the instance variables for the movie object, i.e.
+        movie's title, storyline, poster_image_url and trailer_youtube_url """
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_url
