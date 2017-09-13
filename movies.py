8cf7e0652f2f2eb79dbe6b0f398c24b04b1b98e2
Nikita Sharma <nikitasharma9010@gmail.com>
2017-09-14 00:00:52 +0530

    Create movies.py

---
+++ movies.py
@@ -0,0 +1,24 @@
+# Movies data as a list of dictionaries
+movies = [
+  {
+    "title": "Toy Story",
+    "storyline": "Story of a boy and his toys that come to life",
+    "poster_image_url": "https://upload.wikimedia.org/wikipedia/en/"
+                        "1/13/Toy_Story.jpg",
+    "trailer_youtube_url": "https://www.youtube.com/watch?v=KYz2wyBy3kc"
+  },
+  {
+    "title": "Avatar",
+    "storyline": "A marine on an alien planet",
+    "poster_image_url": "https://upload.wikimedia.org/wikipedia/en/"
+                        "b/b0/Avatar-Teaser-Poster.jpg",
+    "trailer_youtube_url": "https://www.youtube.com/watch?v=cRdxXPV9GNQ"
+  },
+  {
+    "title": "Udaan",
+    "storyline": "",
+    "poster_image_url": "https://upload.wikimedia.org/wikipedia/en/"
+                        "7/71/Udaan_Movie_Poster.jpg",
+    "trailer_youtube_url": "https://www.youtube.com/watch?v=wEJxe2bE-cE"
+  }
+]
